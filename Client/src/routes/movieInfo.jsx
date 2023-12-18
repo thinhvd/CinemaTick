a8f9e1bef923086bf6d94e168e2f2a8696b03f8a
Vuanh1902 <20020128@vnu.edu.vn>
2023-12-18 21:08:27 +0700

    thông tin phim

--- Client/src/routes/movieInfo.jsx
+++ Client/src/routes/movieInfo.jsx
@@ -1,115 +1,70 @@
 import React, { useState } from 'react';
-import { Image, Card, Flex, Typography, Button } from 'antd';
+import { Image, Card, Flex, Typography, Button, Space} from 'antd';
 import TopBar from '../components/topbar';
+import DatVe from '../components/bookingButton';
+
+const { Title, Paragraph } = Typography;
 
 export default function MovieInfo() {
-
+    
     const [poster, setPoster] = useState('');
     const [name, setName] = useState('');
+    const [genre, setGenre] = useState('');
+    const [rate, setRate] = useState('');
     const [description, setDescription] = useState('');
 
-    var movieDetails={
-        "poster": poster,
-        "name": name,
-        "description": description,
-    }
+    // var movieDetails={
+    //     "poster": poster,
+    //     "name": name,
+    //     "description": description,
+    // }
 
-    function getMovieInfo() {
-        fetch("http://fall2324w20g8.int3306.freeddns.org/api/movie/1", {
+    // function getMovieInfo() {
+    //     fetch("http://fall2324w20g8.int3306.freeddns.org/api/movie/1", {
             
-            method: "get",
-        }).then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error));
+    //         method: "get",
+    //     }).then(response => response.json())
+    //         .then(data => console.log(data))
+    //         .catch(error => console.error(error));
+    // }
+
+    const movieDetails={
+        "poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+        "name": "AVENGERS: END GAME",
+        "description": "With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+        "genre": "Action",
+        "rate": "8.4",
     }
 
     return (
         <div className='background'>
             <TopBar />
-            <img
-                alt="avatar"
-                src="/src/images/listMovie/1.jpg"
-                style={{
-                    position:'absolute',
-                    top:'40%',
-                    left:'15%',
-                    scale:'2'
-                }}
-            >
-            </img>
-
-            {/* <Card
-                hoverable
-                style={{ 
-                    position:'absolute',
-                    top:'30%',
-                    left:'10%',right:'30%',
-                    backgroundColor:'transparent',
-                }}
-            >
-                <Flex justify='flex-start'>
-                    <img
-                        alt="avatar"
-                        src="/src/images/listMovie/1.jpg"
-                        
-                    >
-                    </img>
-                    <Flex
-                        vertical
-                        align="flex-start"
-                        justify="space-between"
-                        style={{
-                            padding: 32,
-                        }}
-                    >
-                        <Typography.Title level={1}
-                            style={{
-                                color:'white'
-                            }}
-                        >
-                            AVENGER:END GAME
-                        </Typography.Title>
-                        <Typography.Title level={4}
-                            style={{
-                                color:'white'
-                            }}
-                        >
-                            Description: After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.    
-                            <br/>
-                            Duration: 3
-                        </Typography.Title>
-                        <Button type="primary" href="https://ant.design" target="_blank">
-                            Get Started
-                        </Button>
-                    </Flex>
-                </Flex>
-            </Card> */}
-
-
-
-            {/* <div
-                style={{
-                    height:'90vh',
-                    // border:'none',
-                    backgroundColor:'transparent',
-                    marginTop:'10vh',
-                    color: 'white',
-                    display:'flex',
-                }}
-            >
-                <Image
+                
+            <Space className='movieInfo' direction="horizontal" align="center" >
+                <img
+                    alt="movie poster"
+                    src={movieDetails.poster}
                     className='movieInfoImage'
-                    src="/src/images/listMovie/1.jpg"
-                    preview={false}
                 />
-                <div className='movieInfoText'>
-                    <div id='tenphim'>AVENGER:END GAME</div>
-                    <div id='tomtat'>After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.</div>
-                </div>
-                <Card className='cardDatVe'>
-                    
-                </Card> 
-            </div> */}
+                <Card
+                    hoverable
+                    className='movieInfoText'
+                >
+                    <Space direction="vertical" >
+                        <Title level={1} style={{ color: 'white' }}>
+                            {movieDetails.name}
+                        </Title>
+                        <Paragraph style={{ color: 'white' }}>
+                            <strong>Genre:</strong> {movieDetails.genre}
+                            <br/>
+                            <strong>Rate:</strong> {movieDetails.rate}
+                            <br/>
+                            <strong>Description:</strong> {movieDetails.description}
+                        </Paragraph>
+                        <div style={{position: 'fixed', bottom: '20px', paddingLeft:'10px'}}><DatVe/></div> 
+                    </Space>
+                </Card>
+            </Space>
         </div>
     );
 };
