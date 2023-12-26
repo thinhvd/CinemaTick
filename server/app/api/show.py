4593b586039c644afe61026646fa3f6059e8acce
tq-dat <trinhdat11371@gmail.com>
2023-12-26 11:13:59 +0700

    cpde api bill

--- server/app/api/show.py
+++ server/app/api/show.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, session, jsonify
 from flask_sqlalchemy import SQLAlchemy 
-from app.models import User, Movie, Review, Show, Room, Seat, Ticket, Drink, Bill
+from app.models import User, Movie,  Show, Room, Seat, Ticket, Bill
 from app import db
 from app.api.erorrs import bad_request, error_response
 from app.api.seat import get_seats_by_show_id
@@ -13,6 +13,19 @@ from app.api.seat import create_seat
 def get_show(id):
     show = Show.query.get_or_404(id).to_dict()
     return show
+
+
+
+@bp.route('/api/show/movie/<int:id>', methods=['GET'])
+@cross_origin()
+def get_show_for_movie(id):
+    shows = Show.query.filter_by(movie_id = id)
+    res = []
+    for show in shows:
+        res.append(show.to_dict())
+    return res
+
+
 
 @bp.route('/api/shows', methods=['GET'])
 @cross_origin()
