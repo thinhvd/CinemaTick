11fef44a3e37a394ecdf5ee68160385bfac2bfd0
thinhvd <20021444@vnu.edu.vn>
2023-12-27 13:25:26 +0700

    fix port

--- server/main.py
+++ server/main.py
@@ -5,8 +5,4 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(host='0.0.0.0', port=5001)
-=======
-    app.run(host='0.0.0.0', port=1002)
->>>>>>> 8ee759442f944388b06f246d7b73263c7a0b9e41
