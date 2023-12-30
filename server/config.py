b03dfbf49bc2d58ec4fe2750f9bd8f9e80071f8a
thinhvd <20021444@vnu.edu.vn>
2023-12-30 22:40:44 +0700

    Add VNPAY payment

--- server/config.py
+++ server/config.py
@@ -17,12 +17,21 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     CORS_HEADERS = 'Content-Type'
 
+    #Config token
     JWT_SECRET_KEY = "vu-thong-minh"
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-     
+    
+    #Config mail
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USERNAME = 'cinematickg8@gmail.com'
     MAIL_PASSWORD = 'ctce ytln jmpq mgpb'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
+
+    #Config VNPAY payment
+    VNPAY_RETURN_URL = 'http://localhost:5001/payment_return'  # get from config
+    VNPAY_PAYMENT_URL = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'  # get from config
+    VNPAY_API_URL = 'https://sandbox.vnpayment.vn/merchant_webapi/merchant.html'
+    VNPAY_TMN_CODE = 'NK2LD6FS'  # Website ID in VNPAY System, get from config
+    VNPAY_HASH_SECRET_KEY = 'SMRBLWLQHTTFISTRMYHUMHRUXYNSWJPJ'  # Secret key for create checksum,get from config
